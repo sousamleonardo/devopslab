af865078fa2a00165154d76ea2c80c64ae21fef0
sousamleonardo <65678348+sousamleonardo@users.noreply.github.com>
2022-03-10 14:50:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Leonardo Sousa"
+    return "Hello World - Leonardo Sousa - Sonar"
 
 if __name__ == '__main__':
     app.run()
