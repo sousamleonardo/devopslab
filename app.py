6826a007b715827d13c612b0685981971935fffa
sousamleonardo <65678348+sousamleonardo@users.noreply.github.com>
2022-03-10 14:53:33 -0300

    Update app.py
    Incluindo CSRF Protection

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)
+
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Leonardo Sousa - Sonar"
+    return "Hello World - Leonardo Sousa"
 
 if __name__ == '__main__':
     app.run()
